beefc22a6581568b1b94f7a7a6c422706fdf3e76
sum130 <sm3904@naver.com>
2024-11-23 03:38:41 +0900

    월드프레임 만드는 중
    오류발견.. -> 대기룸 들어갈 때 대기룸 프레임 여러 개 생김

--- BattlePoketmon/src/World.java
+++ BattlePoketmon/src/World.java
@@ -1,5 +1,26 @@
 import java.util.Vector;
 
 public class World {
+	private String roomName = "123";
 	public Vector<Player> users = new Vector<Player>();
+	private int maxNum;
+	private int coinCount = 10;
+	private int id = -1;
+	private int Timer = -1;
+
+
+
+
+
+
+
+	public String getRoomName() {
+		return roomName;
+	}
+
+
+	public int getTimer() {
+		return this.Timer;
+	}
+
 }
---
+++ BattlePoketmon/src/WorldFrame.java
@@ -0,0 +1,92 @@
+import java.awt.BorderLayout;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.SwingConstants;
+
+public class WorldFrame {
+	private JPanel centerPanel;
+	private World worldInfo;
+	private JLabel timerLabel;
+	JFrame frame;
+	
+	JLabel userCountLabel;
+	public WorldFrame(World worldInfo) {
+		this.worldInfo = worldInfo;
+	}
+	
+	public JFrame create() {
+		frame = new JFrame("BattlePoketmon_WORLD");
+		frame.setBounds(200,200,800,600);
+		JPanel entirePanel = new JPanel(new BorderLayout());
+		
+		entirePanel.add(createAbovePanel(), BorderLayout.NORTH);
+		entirePanel.add(createCenterPanel(),BorderLayout.CENTER);
+		frame.add(entirePanel);
+		frame.setVisible(true);
+		return frame;		
+	}
+	
+	public JPanel createAbovePanel() {
+		JPanel abovePanel = new JPanel(new GridLayout(2,3));
+		JLabel roomNameLabel = new JLabel("    <"+worldInfo.getRoomName()+">");
+		JLabel xLabel = new JLabel("");
+		timerLabel = new JLabel("TIMER : " + worldInfo.getTimer() + "          ");
+		timerLabel.setHorizontalAlignment(SwingConstants.RIGHT);
+		
+		abovePanel.add(roomNameLabel);
+		abovePanel.add(xLabel);
+		abovePanel.add(timerLabel);
+		//버튼들
+		JLabel coinLabel= new JLabel("    <COIN> : ");		
+		JButton b_bag = new JButton("가방");
+		JButton b_poketmon = new JButton("포켓몬");
+		
+		b_bag.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+
+			}
+		});
+		b_poketmon.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				
+			}
+		});
+		abovePanel.add(coinLabel);
+		abovePanel.add(b_bag);
+		abovePanel.add(b_poketmon);
+		return abovePanel;
+	}
+	
+	
+	
+	public JPanel createCenterPanel() {
+		centerPanel = new JPanel(new GridLayout(3,2));
+		
+		return centerPanel;
+	}
+	
+	public static void main(String[] args) {
+		World worldInfo = new World();
+		WorldFrame c = new WorldFrame(worldInfo);
+		c.create();
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
